9e94f33180a82d28d04f3be7c54097475608ebce
lijun979598595 <lijun979598595@163.com>
2019-05-28 10:56:27 +0800

    2

--- src/main/java/demo/shiro/servlet/LogoutServlet.java
+++ src/main/java/demo/shiro/servlet/LogoutServlet.java
@@ -14,6 +14,7 @@ public class LogoutServlet extends HttpServlet {
         request.getSession().invalidate();
     }
 
+
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
         doPost(request, response);
     }
